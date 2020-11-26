0a53258fd7db0dbeec7c0073cba7c07a5c7717c9
Quentin Berard <72550698+QuentinBerard@users.noreply.github.com>
2020-11-26 17:16:36 +0100

    Le bon avancement
    pas encore parfait mais presque

--- animation.js
+++ animation.js
@@ -1,7 +1,55 @@
+
+//function date (){
+  //var d = new Date();
+//  var hours = d.getHours();
+  //var minutes =d.getMinutes();
+//  var secondes =d.getSeconds();
+
+//  fullDate =hours + ":" + minutes + ":" + secondes;
+
+
+//}
+var d = new Date();
+var hours = d.getHours();
+var minutes =d.getMinutes();
+var secondes =d.getSeconds();
+
+var allsecondes=hours*3600 + minutes*60 + secondes;
+//setInterval(date, 1000);
+var startcolorR = 1;
+var startcolorV = 1;
+var startcolorB = 2;
+var endcolorR = 144;
+var endcolorV= 50;
+var endcolorB = 27;
+var secondesjour = 86400;
+var newcolor;
+
+function changecolorR (){
+newcolorR=(allsecondes*endcolorR)/secondesjour;
+console.log(newcolorR);
+}
+changecolorR();
+
+function changecolorV (){
+newcolorV=(allsecondes*endcolorV)/secondesjour;
+console.log(newcolorV);
+}
+changecolorV();
+
+function changecolorB (){
+newcolorB=(allsecondes*endcolorB)/secondesjour;
+console.log(newcolorB);
+}
+changecolorB();
+
+newcolor = (changecolorR(),changecolorV(),changecolorB());
+
+console.log(newcolor);
 // let original =anime({
 //     targets:".bgmtg",
 //     fill:"#B4D1CE",
-//     easing: 'linear',   
+//     easing: 'linear',
 // })
 
 // anime({
@@ -15,59 +63,59 @@
 //     <stop  offset="0.5108" style="stop-color:#571948"/>
 //     <stop  offset="1" style="stop-color:#900c8e"/>
 //     `;
-// }  
+// }
 // })
 // // anime({
 // //     targets:".lac",
 // //     fill: "url(#SVGID_2_)",
-// //     easing: 'linear',   
+// //     easing: 'linear',
 // // })
 
 // anime({
 //     targets:".pl5",
 //     fill:"#99C6C4",
-//     easing: 'linear',   
+//     easing: 'linear',
 // })
 
 // anime({
 //     targets:".mtgmil4",
 //     fill:"#72ACB2",
-//     easing: 'linear',   
+//     easing: 'linear',
 // })
 // anime({
 //     targets:".pl3",
 //     fill:"#547B8C",
-//     easing: 'linear',   
+//     easing: 'linear',
 // })
 // anime({
 //     targets:".p2",
 //     fill:"#48657C",
-//     easing: 'linear',   
+//     easing: 'linear',
 // })
 // anime({
 //     targets:".p2dr",
 //     fill:"#61868E",
-//     easing: 'linear',   
+//     easing: 'linear',
 // })
 // // anime({
 // //     targets:".mtglac",
 // //     fill:"url(#SVGID_25_)",
-// //     easing: 'easeInOutExpo',   
+// //     easing: 'easeInOutExpo',
 // // })
 // anime({
 //     targets:".arbresderter",
 //     fill:"#1C2D2D",
-//     easing: 'linear',   
+//     easing: 'linear',
 // })
 // anime({
 //     targets:".terredevant",
 //     fill:"#1C2D2D",
-//     easing: 'linear',   
+//     easing: 'linear',
 // })
 // anime({
 //     targets:".arbresdevant",
 //     fill:"#0f1019",
-//     easing: 'linear',   
+//     easing: 'linear',
 // })
 
 
---
+++ time.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+//if (minutes>43 && minutes<30){
+//console.log(minutes);
+//}
+//else {
+  //console.log(fullDate);
+//}
+//if (hours>=0 && hours<12){
+//console.log("C'est le matin il est"+fullDate);
+//}
+//else {
+  //console.log("C'est l'après-midi il est "+fullDate);
+//}
